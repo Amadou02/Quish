5334daf2895799adacad764257e60edb3ba5695d
SebastienHinard <se.hinard@gmail.com>
2019-05-01 15:21:50 +0200

    ajout de la banniere pub, du pannier et debut de js

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+// quand la page est chargée
+$(document).ready(function(){
+    //on créé un tableau qui contiendra le contenu du panier
+    //chaque article occupera deux index du tableau
+    //le premier renvoi l'id de l'article, le deuxième la quantité
+    var panier = [];
+})
